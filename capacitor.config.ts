a5abc4e340d03598a00ab913be75e62ce2948343
Zaheer Abbas <zaheerstudioz@gmail.com>
2026-02-05 16:39:40 +0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'voicebackend',
+  webDir: 'www'
+};
+
+export default config;
